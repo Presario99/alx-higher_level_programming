05da24fe9201ae0738060c2ba4729009a9495e2d
Frederick Duah <frederickduah13@gmail.com>
2023-11-30 21:51:25 +0000

    Import modules

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,12 +2,10 @@
 import sys
 
 def add_args(args):
-    # converts args to int and sum them up
     result = sum(int(arg) for arg in args)
     return result
 
 if __name__ == '__main__':
-    # Get command line args excluding script name
     args = sys.argv[1:]
 
     result = add_args(args)
