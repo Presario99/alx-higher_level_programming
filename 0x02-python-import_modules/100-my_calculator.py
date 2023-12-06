57995bcf74714982fc46e8063a3e5b1ef4ab8fda
Frederick <frederickduah13@gmail.com>
2023-12-06 16:15:16 +0000

    Python Import Modules

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -25,4 +25,4 @@ if __name__ == '__main__':
         elif sys.argv[2] == "*":
             print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
         elif sys.argv[2] == "/":
-            print("{:d} / {:d} ={:d}".format(a, b, div(a, b)))
+            print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
