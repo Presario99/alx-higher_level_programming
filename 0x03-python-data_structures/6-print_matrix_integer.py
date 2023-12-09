d90f327bfa9813b676ad7156fa1e5648c6e3e6c1
Frederick <frederickduah13@gmail.com>
2023-12-09 01:31:41 +0000

    Python data structures

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
+
 def print_matrix_integer(matrix=[[]]):
-    if not matrix:
-        print()
-    else:
-        for row in range(len(matrix)):
-            for column in range(len(matrix[row])):
-                if column != len(matrix[row]) - 1:
-                     endspace = ' '
-                else:
-                    endspace = ''
-                print("{:d}".format(matrix[row][column], end=endspace))
-            print()
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            print("{:d}".format(matrix[i][j]), end="")
+            if j != (len(matrix[i]) - 1):
+                print(" ", end="")
+
+        print("")
--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
+
 def multiple_returns(sentence):
-    if not sentence:
-        sentence = None
-    if sentence:
-        length = len(sentence)
-    else:
-        length = 0
-    return (length, senetence if not sentence else sentence[:1])
+    if sentence == "":
+        return (0, None)
+    return (len(sentence), sentence[0])
